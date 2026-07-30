23a10c4251242f72d213f8000fab7df7251d6807
Manish27-tech <renu87316@gmail.com>
2026-07-30 10:18:59 +0530

    Add files via upload

--- All Exp/Exp2/src/app/store.js
+++ All Exp/Exp2/src/app/store.js
@@ -1,1 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from '../features/posts/postsSlice';
+import platformsReducer from '../features/platforms/platformsSlice';
+import uiReducer from '../features/ui/uiSlice';
 
+export const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+    platforms: platformsReducer,
+    ui: uiReducer,
+  },
+  devTools: process.env.NODE_ENV !== 'production',
+});
