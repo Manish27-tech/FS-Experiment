637c2f8de1a8e0269a9583c82241bf01ce0caf9b
Manish27-tech <renu87316@gmail.com>
2026-08-11 14:03:50 +0530

    Add files via upload

--- All Exp/Exp3/app/store.js
+++ All Exp/Exp3/app/store.js
@@ -1,1 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from '../features/posts/postsSlice';
+import platformsReducer from '../features/platforms/platformsSlice';
+import uiReducer from '../features/ui/uiSlice';
+import authReducer from '../features/auth/authSlice'; // NEW
 
+export const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+    platforms: platformsReducer,
+    ui: uiReducer,
+    auth: authReducer, // Add auth
+  },
+});
